b4f4e047e2757d303586975d8162c5447400104b
Liz Sugar <liz@lizsugar.me>
2022-02-28 16:19:44 -0800

    Script programmatically produces json from dir
    There may be a json error, i may be misunderstanding the json structure
    of the atla.json example supplied by the original dev

--- generate_json.py
+++ generate_json.py
@@ -4,9 +4,11 @@ from jinja2 import Environment, FileSystemLoader
 import yaml
 import sys
 import os
+import json
 
 template_dir, template_file = os.path.split(sys.argv[1])
 
+# Get our template json file
 env = Environment(
   loader=FileSystemLoader(template_dir),
   trim_blocks=True,
@@ -14,50 +16,49 @@ env = Environment(
 
 final = env.get_template(template_file)
 
-show = {
-  'seasons': {
-    's1': {
-      'slug': 'korra_b1',
-      'name': 'Air',
-      'icon_path': 'empty',
-      'episodes': {
-        'e1': {
-          'slug': 's01e01',
-          'name': 'Welcome to Republic City',
-          'video_filepath': 'videopath',
-          'subititle_filepath': 'subpath'
-        },
-        'e2': {
-          'slug': 's01e02',
-          'name': 'I dunno',
-          'video_filepath': 'videopath',
-          'subititle_filepath': 'subpath'
-        }
-      }
-    },
-
-    's2': {
-      'slug': 'korra_b2',
-      'name': 'Spirits',
-      'icon_path': 'empty',
-      'episodes': {
-        'e1': {
-          'slug': 's02e01',
-          'name': 'poopoo',
-          'video_filepath': 'videopath',
-          'subititle_filepath': 'subpath'
-        },
-        'e2': {
-          'slug': 's02e02',
-          'name': 'peepee',
-          'video_filepath': 'videopath',
-          'subititle_filepath': 'subpath'
-        }
-      }
-    }
-  }
+# Empty dictionary ready for population from show data
+generated_show = {
+  "seasons": [ ]
 }
 
-print(show)
+videoLibrary = sys.argv[2] + "videos"
+subtitleLibrary = sys.argv[2] + "subtitles"
 
-print(final.render(show))
+# Walk our dir
+for dirpath, dirs, files in os.walk(videoLibrary):
+  dirs.sort()
+  if dirpath != videoLibrary:
+    seasonSlug = seasonName = dirpath.split("/")[-1]
+    season = {
+      "seasonSlug": seasonSlug,
+      "seasonName": seasonName,
+      "seasonIcon": "",
+      "episodes": [ ] 
+    }
+
+    for a,b,myfiles in os.walk(dirpath):
+      myfiles.sort()
+
+      for episode in myfiles:
+        episodeSlug = episode.split(" - ")[1]
+        episodeName = episode.split(" - ")[2].split(".")[0]
+        episodeSubtitleFile = episode.split(".")[0] + ".srt"
+        newEpisode = {
+          "episodeSlug": episodeSlug,
+          "episodeName": episodeName,
+          "videoFile": videoLibrary + "/" + episode,
+          "subtitleFile": subtitleLibrary + "/" + episodeSubtitleFile
+        }
+        season["episodes"].append(newEpisode)
+
+    generated_show["seasons"].append(season)
+
+#pretty = json.dumps(generated_show, indent=2)
+#print(type(pretty))
+#print(pretty)
+
+#print(final.render(generated_show))
+output = (final.render(generated_show))
+with open("test.json", "w") as outfile:
+  #json.dump(output, outfile, indent=2)
+  outfile.write(output)
